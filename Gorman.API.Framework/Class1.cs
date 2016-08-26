2aa5e233cc1701c8ebe52a6119f5ee88a8ec459d
Adam Naylor <adamnaylor@gmail.com>
2016-08-26 16:25:48 +0100

    Started github

--- Gorman.API.Framework/Class1.cs
+++ Gorman.API.Framework/Class1.cs
@@ -1,25 +1,44 @@
 ﻿
 namespace Gorman.API.Framework {
+    using System;
+    using System.Threading.Tasks;
     using RestSharp;
 
     public class MapService {
+
+        public MapService(Uri domain) {
+            _restClient = new RestClient(domain);
+            _addMapValidator = new MapValidator();
+        }
 
         public MapService(IRestClient restClient, IMapValidator mapValidator) {
             _restClient = restClient;
             _addMapValidator = mapValidator;
         }
 
-        public async Response Add(Map map) {
+        //todo in the future let people get by GET /user/activity_name
+        public async void Get(int id) {
+            //todo add initialisation code to build list of endpoints
+            var mapsEndpoint = new Uri(MapsEndpoint.Replace("{/map_id}", id.ToString()));
+            var request = new RestRequest(mapsEndpoint, Method.POST);
+            var result = await _restClient.ExecuteTaskAsync<Map>(request);
+            return new Response<Map>(result.Data);
+        }
+
+        public async Task<Response<Map>> Add(Map map) {
             if (!_addMapValidator.IsValidForAdd(map))
-                return;
+                return new Response<Map>();
 
             var request = new RestRequest("/maps", Method.POST);
             request.AddJsonBody(map);
             var result = await _restClient.ExecuteTaskAsync<Map>(request);
+            return new Response<Map>(result.Data);
         }
 
         private readonly IRestClient _restClient;
         private readonly IMapValidator _addMapValidator;
+
+        private const string MapsEndpoint = "/maps{/map_id}";
     }
 
     public class MapValidator
@@ -35,7 +54,14 @@ namespace Gorman.API.Framework {
         
     }
 
-    public class Response {
+    public class Response<T> {
+        public T Result { get; set; }
+
+        public Response() { }
+
+        public Response(T result) {
+            Result = result;
+        }
         
     }
     
